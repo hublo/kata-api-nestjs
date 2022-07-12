a8df596eff8860a8b461ed33b8cf0c4a8732009e
Remy deme <demeremy@gmail.com>
2022-07-12 10:48:01 +0200

    feat(guards)

--- src/app/health/controller/health.controller.ts
+++ src/app/health/controller/health.controller.ts
@@ -1,14 +1,12 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
 import { HealthDto } from './health.dto';
-import { BaseGuard } from '../../../libs/auth/guards/base.guard';
 
 @Controller('health')
 export class HealthController {
   constructor() {}
 
   @Get()
-  @UseGuards(BaseGuard)
   getHealth(): HealthDto {
     return plainToInstance(HealthDto, { status: 'ok' });
   }
